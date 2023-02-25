8e9dad6d543488be997706016c9777523869b23a
kianpas <kianpas@gmail.com>
2023-02-25 23:37:04 +0900

    23.02.25 init commit

---
+++ ShopCommon/src/main/java/com/shop/common/entity/Role.java
@@ -0,0 +1,32 @@
+package com.shop.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ ShopCommon/src/main/java/com/shop/common/entity/User.java
@@ -0,0 +1,62 @@
+package com.shop.common.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",length = 45, nullable = false)
+    private String lastName;
+
+    @Column(length = 64)
+    private String photos;
+
+    private boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Builder
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+
+}
